9e2d16786a71465707277679e952c754899abc70
Dilruba Khan <rubatanisa@yahoo.ie>
2021-11-28 21:42:08 +0000

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,1 @@
-CarrierWave.configure do |config|
-    config.fog_credentials = {
-      provider:              'AWS',                        # required
-      aws_access_key_id:     'AKIA6F2EFOWCJO6R4NUW',       # required unless using use_iam_profile
-      aws_secret_access_key: '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',                        # required unless using use_iam_profile
 
-    }
-    config.fog_directory  = 'ruba-rails-marketplace'            # required
-   
-  end
-  
